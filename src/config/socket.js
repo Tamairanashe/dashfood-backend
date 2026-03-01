cb087404d735075995648138a7b63d6e78e99189
Jothum123 <jothum@gmail.com>
2026-03-01 14:29:25 +0200

    socket

--- src/config/socket.js
+++ src/config/socket.js
@@ -5,7 +5,7 @@ let io;
 const initSocket = (server) => {
     io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173", // Vite default port
+            origin: process.env.FRONTEND_URL || "http://localhost:5173",
             methods: ["GET", "POST"]
         }
     });
