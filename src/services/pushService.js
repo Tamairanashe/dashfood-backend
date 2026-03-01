8fc62d67ec9c4baff1f94aa8be5e61566f4ce8bb
Jothum123 <jothum@gmail.com>
2026-03-01 15:07:07 +0200

    Push services

--- src/services/pushService.js
+++ src/services/pushService.js
@@ -1,23 +1,22 @@
-const { Expo } = require('expo-server-sdk');
-
-const expo = new Expo();
-
 const sendPushNotification = async (pushToken, title, body, data = {}) => {
-    if (!Expo.isExpoPushToken(pushToken)) {
-        console.error(`[Push] Token ${pushToken} is invalid`);
-        return;
-    }
-
-    const message = {
-        to: pushToken,
-        sound: 'default',
-        title,
-        body,
-        data,
-        priority: 'high',
-    };
-
     try {
+        const { Expo } = await import('expo-server-sdk');
+        const expo = new Expo();
+
+        if (!Expo.isExpoPushToken(pushToken)) {
+            console.error(`[Push] Token ${pushToken} is invalid`);
+            return;
+        }
+
+        const message = {
+            to: pushToken,
+            sound: 'default',
+            title,
+            body,
+            data,
+            priority: 'high',
+        };
+
         const ticketChunk = await expo.sendPushNotificationsAsync([message]);
         console.log("[Push] Notification Sent:", ticketChunk);
     } catch (error) {
